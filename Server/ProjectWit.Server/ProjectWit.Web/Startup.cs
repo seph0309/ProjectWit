3047828d421ee78e6615e16848456318390bea12
Joseph <seph0309@gmail.com>
2014-09-03 22:52:57 +0800

    Initial check-in of ASP.NET MVC site

---
+++ Server/ProjectWit.Server/ProjectWit.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectWit.Web.Startup))]
+namespace ProjectWit.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
