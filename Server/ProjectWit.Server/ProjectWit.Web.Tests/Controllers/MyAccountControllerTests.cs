4568580e8ac21425787b431cc3b4a8be66458283
Joseph <seph0309@gmail.com>
2014-09-07 01:43:22 +0800

    Changed naming convention

---
+++ Server/ProjectWit.Server/ProjectWit.Web.Tests/Controllers/MyAccountControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectWit.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ProjectWit.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class MyAccountControllerTests
+    {
+        [TestMethod()]
+        public void GetNavBarTest()
+        {
+            MyAccountController ct = new MyAccountController();
+            var x = ct.GetNavBar();
+            Assert.Fail();
+        }
+    }
+}
--- Server/ProjectWit.Server/ProjectWit.Web/Controllers/MyAccountController.cs
+++ Server/ProjectWit.Server/ProjectWit.Web/Controllers/MyAccountController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectWit.Model;
+using System.Linq;
 
 namespace ProjectWit.Web.Controllers
 {
@@ -14,6 +16,17 @@ namespace ProjectWit.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetNavBar()
+        {
+        
+            using(WITEntities db = new WITEntities())
+            {
+                var nav = (from col in db.Wit_NavBar
+                           select new { Menu = col.Menu, SubMenu = col.SubMenu, URL = col.URL }).ToList();
+                return Json(nav, JsonRequestBehavior.AllowGet);
+            }
         }
 
 	}
