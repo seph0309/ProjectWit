a976ba9736d78b282d14a2007338767e556f32a9
Joseph <seph0309@gmail.com>
2014-09-10 12:36:34 +0800

    Added SaveChanges orverride

--- Server/ProjectWit.Server/ProjectWit.Model/DataExtensions/DBContext.cs
+++ Server/ProjectWit.Server/ProjectWit.Model/DataExtensions/DBContext.cs
@@ -14,25 +14,34 @@
         }
         public override System.Threading.Tasks.Task<int> SaveChangesAsync()
         {
-            var modifiedEntries = this.ChangeTracker.Entries()
-                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
-
-            foreach (var dbEntityEntry in modifiedEntries)
-            {
-                dbEntityEntry.Property("ModifiedDate").CurrentValue = DateTime.UtcNow;
-            }
-
-
+            SetProperties();
             return base.SaveChangesAsync();
         }
 
         public override int SaveChanges()
         {
-            var changeSet = ChangeTracker.Entries();
-
-
+            SetProperties();
             return base.SaveChanges();
         }
 
+        private void SetProperties()
+        {
+            var modifiedEntries = this.ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+
+            foreach (var dbEntityEntry in modifiedEntries)
+            {
+                if (dbEntityEntry.State == EntityState.Added)
+                {
+                    ////Remove GUID's value when saving
+                    //if (dbEntityEntry.Entity.GetType() == typeof(ProjectWit.Model.Wit_Category))
+                    //    dbEntityEntry.Property("Category_UID").CurrentValue = null;
+                }
+                    
+
+                dbEntityEntry.Property("ModifiedDate").CurrentValue = DateTime.Now;
+                dbEntityEntry.Property("ModifiedBy").CurrentValue = "Joseph";
+            }
+        }
     }
 }
