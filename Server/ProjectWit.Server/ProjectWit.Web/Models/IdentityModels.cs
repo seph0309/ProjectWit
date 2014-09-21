689b0dec0c8257495587c3a75847d31392654db7
Joseph <seph0309@gmail.com>
2014-09-21 16:02:39 +0800

    Set ID to all caps

--- Server/ProjectWit.Server/ProjectWit.Web/Models/IdentityModels.cs
+++ Server/ProjectWit.Server/ProjectWit.Web/Models/IdentityModels.cs
@@ -5,6 +5,17 @@ namespace ProjectWit.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public override string Id
+        {
+            get
+            {
+                return base.Id.ToUpper();
+            }
+            set
+            {
+                base.Id = value.ToUpper();
+            }
+        }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
