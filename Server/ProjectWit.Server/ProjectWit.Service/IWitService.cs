9172b4bdc2d870be94cb611d6082d9f312ed56f8
Joseph <seph0309@gmail.com>
2014-10-08 22:42:42 +0800

    WitService - Initial checkin

---
+++ Server/ProjectWit.Server/ProjectWit.Service/IWitService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProjectWit.Service
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    /// <summary>
+    /// Implementation is in ProjectWit.Web
+    /// </summary>
+    [ServiceContract]
+    public interface IWitService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+        
+        [OperationContract]
+        void Test();
+
+        // TODO: Add your service operations here
+    }
+
+}
---
+++ Server/ProjectWit.Server/ProjectWit.Web/WitService.svc.cs
@@ -0,0 +1,39 @@
+﻿using ProjectWit.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProjectWit.Web
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class WitService : IWitService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
