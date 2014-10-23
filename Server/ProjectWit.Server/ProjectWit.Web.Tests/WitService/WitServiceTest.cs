68d928ed0494b9701ae93dd58b1f88f106496b1d
Joseph <seph0309@gmail.com>
2014-10-23 20:08:21 +0800

    Added WitServiceTest

---
+++ Server/ProjectWit.Server/ProjectWit.Web.Tests/WitService/WitServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectWit.Service;
+using ProjectWit.Service.ServiceArguments;
+
+namespace ProjectWit.Web.Tests.WitService
+{
+    [TestClass]
+    public class WitServiceTest
+    {
+        [TestMethod]
+        public void TestSessionServiceBase()
+        {
+            LoginServiceArgs ser = new LoginServiceArgs("SYSADMIN", "PASSWORD", "", "");
+
+            LoginServiceArgs ser2 = new LoginServiceArgs("SYSADMIN", "password", "", "");
+        }
+    }
+}
