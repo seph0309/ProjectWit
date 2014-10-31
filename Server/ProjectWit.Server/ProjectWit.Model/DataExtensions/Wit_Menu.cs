2fa07c00448b21939fe5666b2072c63601059d31
Joseph <seph0309@gmail.com>
2014-10-31 20:00:24 +0800

    Added Wit_Menu

---
+++ Server/ProjectWit.Server/ProjectWit.Model/DataExtensions/Wit_Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWit.Model
+{
+    public static class Wit_Menu
+    {
+        public const string AdminMaintenance = "Admin Maintenance";
+        public const string MyProfile = "My Profile";
+    }
+}
