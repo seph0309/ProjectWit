81e8805ccb4dc3131c317ce6f6523c94166d61a3
Joseph <seph0309@gmail.com>
2014-11-30 18:17:44 +0800

    refactor

--- Server/ProjectWit.Server/ProjectWit.Service/ServiceArguments/TransactionServiceArgs.cs
+++ Server/ProjectWit.Server/ProjectWit.Service/ServiceArguments/TransactionServiceArgs.cs
@@ -40,9 +40,7 @@ namespace ProjectWit.Service.ServiceArguments
         
         public void GetTransaction(string transactionID)
         {
-            Transaction = ITransaction.GetByIdAsync(new Guid(transactionID)).Result;
-            if (Transaction == null)
-                LogMsg("Transaction not found");
+            Transaction = ITransaction.FindByIdAsync(new Guid(transactionID)).Result;
         }
 
         public void SetTransactionStatus(string transactionID, string status)
